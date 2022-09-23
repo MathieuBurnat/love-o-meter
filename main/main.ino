76706ee930c8919941afb77e375f6af6332b12a5
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-09-23 12:29:33 +0200

    [Refactor] remove the bad tmp calcul

--- main/main.ino
+++ main/main.ino
@@ -1,48 +1,47 @@
-const int sensorTMP = A0; //set tmp sensor to the a0 pin
-const float baselineTemp = 21.0; // room temperature
+const int sensorTMP = A0;         //set tmp sensor to the a0 pin
+const float baselineTemp = 16.0;  // room temperature
 
 void setup() {
-  Serial.begin(9600); // open se port to comunicate with Arduino
+  Serial.begin(9600);  // open se port to comunicate with Arduino
 
-  //set LED as output and its state to LOW 
-  for(int pn = 2; pn<5; pn++){
+  //set LED as output and its state to LOW
+  for (int pn = 2; pn < 5; pn++) {
     pinMode(pn, OUTPUT);
     digitalWrite(pn, LOW);
   }
 }
 
 void loop() {
-  int sensorValue = analogRead(sensorTMP); //in celcius
-  float voltage = (sensorValue / 1024.0) * 5.0; // Convert ADC(Analog to Digital Converter) as tension
+  int sensorValue = analogRead(sensorTMP);       //in celcius
+  float voltage = (sensorValue / 1024.0) * 5.0;  // Convert ADC(Analog to Digital Converter) as tension
   float temperature = (voltage - 0.5) * 100;
+
 
   Serial.print("sensor value :");
   Serial.print(sensorValue);
-  
+
   Serial.print(", volts :");
   Serial.print(voltage);
-  
+
   Serial.print(", Current temperature :");
   Serial.print(temperature);
 
-  Serial.print(", baline temperature :");
+  Serial.print(", baseline temperature :");
   Serial.println(baselineTemp);
-  
-    if(temperature < baselineTemp+2){
+
+  if (temperature < baselineTemp + 2) {
     digitalWrite(2, LOW);
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
-  }
-  else if(temperature >= baselineTemp+4 && temperature < baselineTemp+6){
+  } else if (temperature >= baselineTemp + 4 && temperature < baselineTemp + 6) {
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
-    digitalWrite(4, LOW);    
-  }
-  else if(temperature >= baselineTemp+6){
+    digitalWrite(4, LOW);
+  } else if (temperature >= baselineTemp + 6) {
     digitalWrite(2, HIGH);
     digitalWrite(3, HIGH);
-    digitalWrite(4, HIGH);    
+    digitalWrite(4, HIGH);
   }
-  
+
   delay(2000);
 }
